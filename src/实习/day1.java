61e2fef165b487f38fd58975dc6310a3ad51e7fb
dbdxcs <654264536@qq.com>
2019-07-18 15:18:19 +0800

    shuru

---
+++ src/实习/day1.java
@@ -0,0 +1,17 @@
+package ʵϰ;
+
+public class day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+for(int i=0;i<36;i++) {
+	int x=2*i+4*(35-i);
+	if(x==94) {
+		System.out.println(i);
+		System.out.println(35-i);
+	}
+}
+	}
+
+}
